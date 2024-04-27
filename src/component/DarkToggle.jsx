c2e34d0a69e85f1e1834eb5ce86e55aa739a8ab5
bal16 <ahmadhamidbalya@gmail.com>
2024-04-27 09:31:50 +0700

    use fontawesome icons

--- src/component/DarkToggle.jsx
+++ src/component/DarkToggle.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWandSparkles} from "@fortawesome/free-solid-svg-icons"
+import { faMoon, faSun} from "@fortawesome/free-regular-svg-icons"
 
 const isChecked = () => {
   const a = localStorage.theme == "halloween" ? "dark" : "";
@@ -21,7 +24,7 @@ export const DarkToggle = () => {
     <>
       <label
         className=" backdrop-blur-lg bg-opacity-50 swap swap-rotate fixed rounded-full btn right-5 z-[99] bottom-4 shadow-sm p-7 bg-base-300  duration-1000 border-none tooltip tooltip-left shadow-base-content"
-        data-tip={auto ? "revelio!" : dark ? "lumos!" : "nox!"}
+        data-tip={!localStorage.theme&&auto ? "revelio!" : dark ? "lumos!" : "nox!"}
       >
         {/* this hidden checkbox controls the state */}
 
@@ -34,36 +37,17 @@ export const DarkToggle = () => {
           className="theme-controller"
           id="tombolDark"
         />
-        
+
         {/* half icon */}
-        {auto ? (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={24} className="absolute fill-current end-4 bottom-4"><path d="M464 6.1c9.5-8.5 24-8.1 33 .9l8 8c9 9 9.4 23.5 .9 33l-85.8 95.9c-2.6 2.9-4.1 6.7-4.1 10.7V176c0 8.8-7.2 16-16 16H384.2c-4.6 0-8.9 1.9-11.9 5.3L100.7 500.9C94.3 508 85.3 512 75.8 512c-8.8 0-17.3-3.5-23.5-9.8L9.7 459.7C3.5 453.4 0 445 0 436.2c0-9.5 4-18.5 11.1-24.8l111.6-99.8c3.4-3 5.3-7.4 5.3-11.9V272c0-8.8 7.2-16 16-16h34.6c3.9 0 7.7-1.5 10.7-4.1L464 6.1zM432 288c3.6 0 6.7 2.4 7.7 5.8l14.8 51.7 51.7 14.8c3.4 1 5.8 4.1 5.8 7.7s-2.4 6.7-5.8 7.7l-51.7 14.8-14.8 51.7c-1 3.4-4.1 5.8-7.7 5.8s-6.7-2.4-7.7-5.8l-14.8-51.7-51.7-14.8c-3.4-1-5.8-4.1-5.8-7.7s2.4-6.7 5.8-7.7l51.7-14.8 14.8-51.7c1-3.4 4.1-5.8 7.7-5.8zM87.7 69.8l14.8 51.7 51.7 14.8c3.4 1 5.8 4.1 5.8 7.7s-2.4 6.7-5.8 7.7l-51.7 14.8L87.7 218.2c-1 3.4-4.1 5.8-7.7 5.8s-6.7-2.4-7.7-5.8L57.5 166.5 5.8 151.7c-3.4-1-5.8-4.1-5.8-7.7s2.4-6.7 5.8-7.7l51.7-14.8L72.3 69.8c1-3.4 4.1-5.8 7.7-5.8s6.7 2.4 7.7 5.8zM208 0c3.7 0 6.9 2.5 7.8 6.1l6.8 27.3 27.3 6.8c3.6 .9 6.1 4.1 6.1 7.8s-2.5 6.9-6.1 7.8l-27.3 6.8-6.8 27.3c-.9 3.6-4.1 6.1-7.8 6.1s-6.9-2.5-7.8-6.1l-6.8-27.3-27.3-6.8c-3.6-.9-6.1-4.1-6.1-7.8s2.5-6.9 6.1-7.8l27.3-6.8 6.8-27.3c.9-3.6 4.1-6.1 7.8-6.1z"/></svg>
+        {!localStorage.theme&&auto ? (
+        <>
+          <FontAwesomeIcon icon={faWandSparkles} className="absolute h-6 fill-current end-4 bottom-4 swap-on" />
+          <FontAwesomeIcon icon={faWandSparkles} className="absolute h-6 fill-current end-4 bottom-4 swap-off" />
+          </>
         ) : (
-          /* sun icon */
-          /* moon icon */
           <>
-            <svg
-              className="absolute fill-current swap-on end-4 bottom-4"
-              xmlns="httxp://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-            </svg>
-            <svg
-              className="absolute fill-current swap-off end-4 bottom-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d={
-                  "M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
-                }
-              />
-            </svg>
+            <FontAwesomeIcon icon={faMoon} className="absolute h-6 fill-current swap-on end-5 bottom-4" />
+            <FontAwesomeIcon icon={faSun}  className="absolute h-6 fill-current swap-off end-4 bottom-4"/>
           </>
         )}
       </label>
