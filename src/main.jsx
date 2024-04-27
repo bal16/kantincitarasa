895478fc48e66f9791f92b72f81d80f0bfdb577e
bal16 <ahmadhamidbalya@gmail.com>
2024-04-27 08:42:38 +0700

    fix dark mode logic & fix tailwind import

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./component/App.jsx";
 
-
 // import App from './component/App.jsx'
-// import './css/index.css'
+import './css/App.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container:{
+    container: {
       // center: true,
     },
     extend: {},
   },
   daisyui: {
     themes: ["bumblebee", "halloween"],
+    darkTheme: "halloween",
+    // lightTheme: "bumblebee",
+    themeRoot: ":root"
+    // themes: [{"light" : "bumblebee"}, {"dark":"halloween"}],
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('daisyui')],
-}
-
+    require("daisyui"),
+  ],
+  // daisyui: {
+  //   themes: [
+      
+  //   ],
+  // },
+};
